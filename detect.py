a0e0ce1b81e61a08d2581d6b66139a01526605af
Youngman <61394223+YoungmanCH@users.noreply.github.com>
2024-01-19 09:42:58 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,9 +1,5 @@
 from ultralytics import YOLO
 
-# モジュールが存在するディレクトリのパス
-module_dir = '/Users/youngman/practice/AI/ultralytics/ultralytics/engine/'
-
-
 model = YOLO("yolov8n-seg.pt")
 
     # Process the frame with YOLO model
